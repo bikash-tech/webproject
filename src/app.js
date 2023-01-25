63a14e69a2f5c9f87ff0bc227200d4d1b7f0e05f
bikash328 <bikash.panigrahi@goniyo.com>
2023-01-25 07:45:05 +0530

    nodejs project first commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const hbs = require('hbs');
+const port = process.env.PORT || 3000;
+
+// public static path
+const staticPath = path.join(__dirname, "../public");
+// use static page
+app.use(express.static(staticPath));
+
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// views
+app.set('view engine', 'hbs')
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Routing
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/about", (req, res) => {
+    res.render("about");
+})
+
+app.get("/weather", (req, res) => {
+    res.render("weather");
+})
+
+// universal error page if path not found
+app.get("*", (req, res) => {
+    res.render("404error");
+})
+
+app.listen(port, () => {
+    console.log('server started..')
+})
